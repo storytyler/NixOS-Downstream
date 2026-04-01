6f008713b323561c73d627be690c0a50ac6e5494
author <tmoneyrolling@gmail.com>
2026-04-01 11:44:35 -0500

    Add common libraries to nix-ld module

--- modules/core/nix-ld.nix
+++ modules/core/nix-ld.nix
@@ -1,4 +1,14 @@
 { pkgs, ... }:
 {
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      # Common libraries needed by non-Nix binaries
+      stdenv.cc.cc
+      zlib
+      zstd
+      curl
+      openssl
+    ];
+  };
 }
