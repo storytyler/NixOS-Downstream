fc455c96b36c3182c0c78fd444f2ea38afe757de
tmoneyrolling@gmail.com <tmoneyrolling@gmail.com>
2026-05-06 16:52:09 -0500

    Added packages that Opencode Nix skills depend on, created module for optnix to allow the tool to function

---
+++ modules/core/optnix.nix
@@ -0,0 +1,20 @@
+{ inputs, options, pkgs, host, ... }:
+let
+  optnixLib = inputs.optnix.mkLib pkgs;
+in
+{
+  imports = [ inputs.optnix.nixosModules.optnix ];
+
+  programs.optnix = {
+    enable = true;
+    settings = {
+      scopes = {
+        ${host} = {
+          description = "NixOS configuration for ${host}";
+          options-list-file = optnixLib.mkOptionsList { inherit options; };
+          evaluator = "nix eval ~/NixOS#nixosConfigurations.${host}.config.{{ .Option }}";
+        };
+      };
+    };
+  };
+}
