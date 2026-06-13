77b50031c70274d330c6f144cb001ed2e20cab41
tmoneyrolling@gmail.com <tmoneyrolling@gmail.com>
2026-06-13 15:56:52 -0500

    nix flake update

--- modules/desktop/hyprland/scripts/keybinds.nix
+++ modules/desktop/hyprland/scripts/keybinds.nix
@@ -42,6 +42,7 @@ pkgs.writeShellScriptBin "keybinds" ''
     "SUPER SHIFT S" "Launch spotify" "spotify" \
     "SUPER SHIFT Y" "Launch youtube-music" "youtube-music" \
     "SUPER SHIFT P" "Launch Pokemon Infinite Fusion" "wine ~/.games/InfiniteFusion.exe" \
+    "SUPER CTRL SHIFT P" "Launch Pokemon Infinite Fusion: Hoenn" "wine ~/.games/Hoenn/InfiniteFusion.exe" \
     "SUPER B" "Launch factcheck" "launcher factcheck" \
     "SUPER ALT Tab" "Window switcher (all workspaces)" "launcher window" \
     "CTRL ALT Delete" "Open system monitor" "$_terminal -e 'btop'" \
